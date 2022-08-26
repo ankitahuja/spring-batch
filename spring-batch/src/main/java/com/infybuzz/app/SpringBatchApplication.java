0f53a01889a444b2d76109743b7a83b93313a351
ahuja <ahuja@LAPTOP-S2BI449H>
2022-08-26 13:37:07 +0530

    Commit 1

--- spring-batch/src/main/java/com/infybuzz/app/SpringBatchApplication.java
+++ spring-batch/src/main/java/com/infybuzz/app/SpringBatchApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
+		System.out.println("In main application");
 		SpringApplication.run(SpringBatchApplication.class, args);
 	}
 
